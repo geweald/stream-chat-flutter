1ae2319127c8f72b04e608bd11164bf07ebcf889
geweald <misieekq@gmail.com>
2022-05-26 13:54:11 +0200

    (fix:ui) web video attachments

--- packages/stream_chat_flutter/lib/src/message_input.dart
+++ packages/stream_chat_flutter/lib/src/message_input.dart
@@ -1422,7 +1422,7 @@ class MessageInputState extends State<MessageInput> {
             StreamVideoThumbnailImage(
               height: 104,
               width: 104,
-              video: (attachment.file?.path ?? attachment.assetUrl)!,
+              video: attachment.file?.path ?? attachment.assetUrl,
               fit: BoxFit.cover,
             ),
             Positioned(
